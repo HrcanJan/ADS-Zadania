4488b340d6d598adb30acee1d8da8a876687f85f
Hrćan Jan <xhrcan@stuba.sk>
2023-03-23 12:46:08 +0100

    refactor: small changes

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 def load(name):
     """
     Load the data
@@ -16,6 +17,7 @@ def load(name):
 
     return arr
 
+
 def takeSecond(arr):
     """
     Used as a key to return the 2nd element in an array
@@ -24,6 +26,7 @@ def takeSecond(arr):
     arr:    input array
     """
     return arr[1]
+
 
 def generate_params(textArr):
     """
@@ -36,33 +39,33 @@ def generate_params(textArr):
     """
     textArr.sort(key=takeSecond)
     sum_f = sum([textArr[i][0] for i in range(len(textArr))])
+    n = len(textArr)
 
-    pi = []
-    qi = []
-    str = []
-    dummy_f = 0
-    p = [0]
-    s = [""]
+    pi, qi, str = [], [], []
+    p, s, dummy = [0], [""], 0
 
-    for i in range(len(textArr)):
-        if textArr[i][0] > 50000:
+    for i in textArr:
+        if i[0] > 50000:
+            pi.append(p[0])
+            qi.append(dummy / sum_f)
+            str.append(s[0])
 
-            pi.append(p.pop())
-            qi.append(dummy_f / sum_f)
-            str.append(s.pop())
-            p.append(textArr[i][0] / sum_f)
-            s.append(textArr[i][1])
-            dummy_f = 0
+            p.append(i[0] / sum_f)
+            s.append(i[1])
+
+            dummy = 0
+            p.pop(0)
+            s.pop(0)
         else:
-            dummy_f += textArr[i][0]
+            dummy += i[0]
 
-    pi.append(p.pop())
-    qi.append(dummy_f / sum_f)
-    str.append(s.pop())
+    pi.append(p[0])
+    qi.append(dummy / sum_f)
+    str.append(s[0])
     return pi, qi, str
 
 
-def generate_tree(pi, qi):
+def generate_tree(pi, qi, n):
     """
     Generate root and e matrix based on algorithm described in Optimal-BST str. 419 15.5 Introduction to Algorithms
     Code inspired from: https://www.geeksforgeeks.org/optimal-binary-search-tree-dp-24/
@@ -71,34 +74,34 @@ def generate_tree(pi, qi):
     params:
     pi:     probability of selecting a word from a sorted list of words with frequency greater than 50000.
     qi:     probability of selecting a word from the low-frequency words after selecting a word from the high-frequency words
+    n:      size of pi
     """
-    n = len(pi)
-
+    cost = [[0 for i in range(n)] for j in range(n + 1)]
+    prob_sum = [[0 for i in range(n)] for j in range(n + 1)]
     root = [[0 for i in range(n)] for j in range(n)]
-    prob_sum = [[0 for i in range(n)] for i in range(n + 1)]
-    expect_cost = [[0 for i in range(n)] for i in range(n + 1)]
 
     for i in range(1, n + 1):
+        cost[i][i - 1] = qi[i - 1]
         prob_sum[i][i - 1] = qi[i - 1]
-        expect_cost[i][i - 1] = qi[i - 1]
 
     for l in range(1, n):
         for i in range(1, n - l + 1):
             j = i + l - 1
-            expect_cost[i][j] = math.inf
+            cost[i][j] = math.inf
             prob_sum[i][j] = prob_sum[i][j - 1] + pi[j] + qi[j]
-            for r in range(i, j + 1):
 
-                t = expect_cost[i][r - 1] + expect_cost[r + 1][j] + prob_sum[i][j]
-                if t < expect_cost[i][j]:
-                    expect_cost[i][j] = t
+            for r in range(i, j + 1):
+                t = cost[i][r - 1] + cost[r + 1][j] + prob_sum[i][j]
+                if t < cost[i][j]:
+                    cost[i][j] = t
                     root[i][j] = r
-    return root
+    return cost, root
 
 
 def search_tree(root, str, start, end, s, count):
     """
     Search the tree recursively and calculate the dept (i.e. number of comparisons) and return the results
+    If the given word is not found, it returns (None, depth)
     
     params: 
     root:      generated tree
@@ -108,40 +111,32 @@ def search_tree(root, str, start, end, s, count):
     s:         string representing the key being searched in the current subtree
     count:     counter of the number of depth
     """
-    if start > end or end < 1:
-        return None, count
-    if root[start][end] == None:
+    if start > end or end <= 0 or root[start][end] == None:
         return None, count
 
     root_s = str[root[start][end]]
 
     if (not root_s) or (len(root_s) == 0) or (root_s == s):
-        count += 1
-        return root_s, count
+        return root_s, count + 1
+    
     if s < root_s:
-        count += 1
-        return search_tree(root, str, start, root[start][end] - 1, s, count)
-    count += 1
-
-    return search_tree(root, str, root[start][end] + 1, end, s, count)
+        return search_tree(root, str, start, root[start][end] - 1, s, count + 1)
+    
+    return search_tree(root, str, root[start][end] + 1, end, s, count + 1)
 
 
 def pocet_porovnani(s, root, str):
     """
     Returns a number of comparisons for a given word
-    If the given word is not found, it returns (None, depth)
 
     params: 
     root:      generated tree
     str:       array of keys with high frequency
     s:         string representing the key being searched in the current subtree
     """
-    count = 0
     if len(s) == 0:
-        return None, count
-    else:
-        found_key, count = search_tree(root, str, 1, len(root) - 1, s, count)
-
+        return None, 0
+    found_key, count = search_tree(root, str, 1, len(root) - 1, s, 0)
     return found_key, count
 
 
@@ -155,26 +150,22 @@ def branches(root, str):
     """
     branches = []
     for s in str:
-        key = []
         s, branch = pocet_porovnani(s, root, str)
-        key.append(s)
-        key.append(branch)
-        branches.append(key)
+        branches.append([s, branch])
 
     for i in branches:
         if not(i[0]):
             branches.remove(i)
 
     branches.sort(key=takeSecond)
-    c_level = 0
-    n_level = 0
-    for branch in branches:
+    current_level = 0
 
-        n_level = branch[1]
-        if n_level > c_level:
-            c_level = n_level
+    for branch in branches:
+        level = branch[1]
+        if(current_level < level):
+            current_level = level
             if(branch[1] == 1):
-                print(f"Root {branch[1]}:")
+                print(f"Root:")
                 print(f"\t-{branch[0]}")
                 continue
             print(f"Branch {branch[1]}:")
@@ -184,7 +175,7 @@ def branches(root, str):
 if __name__ == "__main__":
     textArr = load("dictionary.txt")
     pi, qi, str = generate_params(textArr)
-    root = generate_tree(pi, qi)
+    cost, root = generate_tree(pi, qi, len(pi))
     branches(root, str)
 
     while True:
